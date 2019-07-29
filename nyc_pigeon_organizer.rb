780f386c925e7822efbde25a570f8a00e2d9c709
Shane Lonergan <sptlonergan@gmail.com>
2019-07-29 20:27:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash_by_name = {}
+  data.each do | category, hash |
+    hash.each do |attribute, names|
+      names.each do |name|
+        binding.pry
+        if hash_by_name.has_key?(name)
+          hash_by_name[name][category] >> attribute.to_s
+          #binding.pry
+        else
+          hash_by_name[name][category] = attribute.to_s
+          #binding.pry
+        end
+      end
+    end
+  end
+
+  return hash_by_name
 end
