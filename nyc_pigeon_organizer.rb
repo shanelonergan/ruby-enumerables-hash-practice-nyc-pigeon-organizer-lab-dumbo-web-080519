689c6bfa0c4b57b99bcb0253be489d8dc82e5c98
Shane Lonergan <sptlonergan@gmail.com>
2019-07-29 20:49:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash_by_name = {}
+  data.each do | category, hash |
+    hash.each do |attribute, names|
+      names.each do |name|
+        #binding.pry
+        if hash_by_name.has_key?(name)
+          if hash_by_name[name].has_key?(category)
+            hash_by_name[name][category] << attribute.to_s
+          else
+            hash_by_name[name][category] = []
+            hash_by_name[name][category] << attribute.to_s
+          end
+
+          #binding.pry
+        else
+          hash_by_name[name] = {}
+          hash_by_name[name][category] = []
+          hash_by_name[name][category] << attribute.to_s
+          #binding.pry
+        end
+      end
+    end
+  end
+
+  return hash_by_name
 end
